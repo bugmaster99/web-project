c695637d8073767d2e4fa204e02eefee8017fb0c
Yash Kelkar <yashkelkar@virohan.org>
2025-02-13 11:52:19 +0530

    true condition

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
             onChange={onMobileChange}
             placeholder="Enter mobile number"
           />
-          {isOtpInputVisible && (
+          {true && (
             <>
               <p>Enter OTP:</p>
               <input
